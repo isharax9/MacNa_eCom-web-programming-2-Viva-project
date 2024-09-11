b4e8df1d87bbea5a7871c3459b64d05c890feffb
mac_knight141 <123813453+isharax9@users.noreply.github.com>
2024-09-12 00:11:13 +0530

    single-product.html page front end data can now fetch on DB✅
    📌all the data can now fetch on db including 3 imgs

--- web/js/single-product.js
+++ web/js/single-product.js
@@ -41,14 +41,35 @@ async function loadProduct() {
             const id = json.product.id;
             const imagePathBase = "product-images/" + id;
 
-            
-            
+
+
             // Update thumanil src
             document.getElementById("thum1").src = imagePathBase + "/image1.png";
             document.getElementById("thum2").src = imagePathBase + "/image2.png";
             document.getElementById("thum3").src = imagePathBase + "/image3.png";
 
-            
+            document.getElementById("product-title").innerHTML = json.product.title;
+            document.getElementById("product-published-on").innerHTML = json.product.date_time;
+
+            document.getElementById("product-price").innerHTML = new Intl.NumberFormat(
+                    "en-US",
+                    {
+                        minimumFractionDigits: 2
+                    }
+            ).format(json.product.price);
+            document.getElementById("product-category").innerHTML = json.product.model.category.name;
+
+            document.getElementById("product-model").innerHTML = json.product.model.name;
+            document.getElementById("product-condition").innerHTML = json.product.product_condition.name;
+            document.getElementById("product-qty").innerHTML = json.product.qty;
+
+            document.getElementById("product-color").innerHTML = json.product.color.name;
+//            document.getElementById("color-border").style.borderColor = json.product.color.name;
+//            document.getElementById("color-background").style.backgroundColor = json.product.color.name;
+
+            document.getElementById("product-storage").innerHTML = json.product.storage.value;
+            document.getElementById("product-description").innerHTML = json.product.description;
+
 
         } else {
             window.location = "index.html";
