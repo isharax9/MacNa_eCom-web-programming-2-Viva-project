7a2a9cfc3ee34e4664e970d7c722e509153c7608
mac_knight141 <123813453+isharax9@users.noreply.github.com>
2024-09-12 13:19:16 +0530

    previous version bugs 🐛 fixed ✅
    📌newly clean and build version
    📌added raw cart.html file
    📌Cart DTO added
    📌Fixed ✅✅--->❌single product js is not fully stable. but it can do the work for this stage. (giving SyntaxError: Unexpected end of JSON input at addToCart ....)❌

--- src/java/controller/AddToCart.java
+++ src/java/controller/AddToCart.java
@@ -1,15 +1,20 @@
 package controller;
 
+import com.google.gson.Gson;
+import dto.Cart_DTO;
+import dto.Response_DTO;
 import dto.User_DTO;
 import entity.Cart;
 import entity.Product;
 import entity.User;
 import java.io.IOException;
+import java.util.ArrayList;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import model.HibernateUtil;
 import model.Validation;
 import org.hibernate.Criteria;
@@ -22,21 +27,31 @@ public class AddToCart extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        Response_DTO response_DTO = new Response_DTO();
         
+        
+        Gson gson  = new Gson();
+        
+
         Session session = HibernateUtil.getSessionFactory().openSession();
         Transaction transaction = session.beginTransaction();
 
         try {
             String id = request.getParameter("id");
             String qty = request.getParameter("qty");
-            
+
             System.out.println(id);
             System.out.println(qty);
 
             if (!Validation.isInteger(id)) {
                 //prod not found
+                response_DTO.setContent("Product not Found");
+
             } else if (!Validation.isInteger(qty)) {
-                //invalid qty
+                //invalid quantity
+                response_DTO.setContent("invalid quantity");
+
             } else {
 
                 int productId = Integer.parseInt(id);
@@ -44,6 +59,7 @@ public class AddToCart extends HttpServlet {
 
                 if (productQty <= 0) {
                     //qty must be greater than 0
+                    response_DTO.setContent("qty must be greater than 0");
 
                 } else {
                     Product product = (Product) session.load(Product.class, productId);
@@ -76,47 +92,125 @@ public class AddToCart extends HttpServlet {
                                     cart.setProduct(product);
                                     cart.setQty(productQty);
                                     cart.setUser(user);
-                                    
+
                                     session.save(cart);
                                     transaction.commit();
 
+                                    response_DTO.setSuccess(true);
+                                    response_DTO.setContent("Product Added to the cart");
                                 } else {
                                     //qty not available
+                                    response_DTO.setContent("qty not available");
                                 }
 
                             } else {
                                 //item already found in cart
-                                
+
                                 Cart cartItem = (Cart) criteria2.uniqueResult();
-                                
-                                if ((cartItem.getQty()+productQty) <= product.getQty()) {
-                                    
-                                    cartItem.setQty(cartItem.getQty()+productQty);
+
+                                if ((cartItem.getQty() + productQty) <= product.getQty()) {
+
+                                    cartItem.setQty(cartItem.getQty() + productQty);
                                     session.update(cartItem);
                                     transaction.commit();
-                                    
+
                                 } else {
                                     //can't update your cart.quantity not available.
-                                    
-                                    
+                                    response_DTO.setContent("can't update your cart.quantity not available.");
+
                                 }
-                                
+
                             }
 
                         } else {
                             //Session Cart
+
+                            HttpSession httpSession = request.getSession();
+
+                            if (httpSession.getAttribute("sessionCart") != null) {
+                                //session cart found
+
+                                ArrayList<Cart_DTO> sessionCart = (ArrayList<Cart_DTO>) httpSession.getAttribute("sessionCart");
+
+                                Cart_DTO foundCart_DTO = null;
+
+                                for (Cart_DTO cart_DTO : sessionCart) {
+                                    if (cart_DTO.getProduct().getId() == product.getId()) {
+                                        foundCart_DTO = cart_DTO;
+                                        break;
+                                    }
+                                }
+
+                                if (foundCart_DTO != null) {
+                                    //product found
+
+                                    if ((foundCart_DTO.getQty() + productQty) <= product.getQty()) {
+                                        //update qty
+
+                                        foundCart_DTO.setQty(foundCart_DTO.getQty() + productQty);
+
+                                        response_DTO.setSuccess(true);
+                                        response_DTO.setContent("Product Added to the cart");
+
+                                    } else {
+                                        //quantity not available
+                                        response_DTO.setContent("quantity not available");
+                                    }
+
+                                } else {
+                                    //product not found
+
+                                    if (productQty <= product.getQty()) {
+                                        //add to session cart
+
+                                        Cart_DTO cart_DTO = new Cart_DTO();
+                                        cart_DTO.setProduct(product);
+                                        cart_DTO.setQty(productQty);
+                                        sessionCart.add(cart_DTO);
+
+                                        response_DTO.setSuccess(true);
+                                        response_DTO.setContent("Product Added to the cart");
+
+                                    } else {
+                                        //quantity not available
+                                        response_DTO.setContent("quantity not available");
+                                    }
+                                }
+
+                            } else {
+                                //session cart not found
+
+                                if (productQty <= product.getQty()) {
+                                    //add to session cart
+
+                                    ArrayList<Cart_DTO> sessionCart = new ArrayList<>();
+
+                                    Cart_DTO cart_DTO = new Cart_DTO();
+                                    cart_DTO.setProduct(null);
+                                    cart_DTO.setQty(0);
+                                    sessionCart.add(cart_DTO);
+
+                                    httpSession.setAttribute("sessionCart", sessionCart);
+                                } else {
+                                    //quantity not available
+                                    response_DTO.setContent("quantity not available");
+                                }
+                            }
                         }
                     } else {
                         //product not found
+                        response_DTO.setContent("product not found");
                     }
                 }
             }
-
         } catch (Exception e) {
             // handle the exception
-            // log the exception
-            System.err.println("An error occurred: " + e.getMessage());
             e.printStackTrace();
+            System.err.println("An error occurred: " + e.getMessage());
+            
         }
+        
+        response.setContentType("application/json");
+        response.getWriter().write(gson.toJson(response_DTO));
     }
 }
