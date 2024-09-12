d74a6387ec17e8cab125787174604f2597f540ad
mac_knight141 <123813453+isharax9@users.noreply.github.com>
2024-09-12 20:40:42 +0530

    🛒 DB cart and session carts are now been fully synchronize with DB ✅🚀
    📌SignIn Controller logics updated with both session and db carts

--- src/java/controller/SignIn.java
+++ src/java/controller/SignIn.java
@@ -2,83 +2,143 @@ package controller;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import dto.Cart_DTO;
 import dto.Response_DTO;
 import dto.User_DTO;
+import entity.Cart;
 import entity.User;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import model.HibernateUtil;
-
 import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.criterion.Restrictions;
 
-
-/**
- *
- * @author isharaLakshitha
- */
 @WebServlet(name = "SignIn", urlPatterns = {"/SignIn"})
 public class SignIn extends HttpServlet {
-
+    
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        
         Response_DTO response_DTO = new Response_DTO();
-
+        
         Gson gson = new GsonBuilder().excludeFieldsWithoutExposeAnnotation().create();
         User_DTO user_DTO = gson.fromJson(request.getReader(), User_DTO.class);
-
+        
         if (user_DTO.getEmail().isEmpty()) {
             response_DTO.setContent("Please enter your email");
-
+            
         } else if (user_DTO.getPassword().isEmpty()) {
             response_DTO.setContent("Please enter your password");
-
+            
         } else {
-
+            
             Session session = HibernateUtil.getSessionFactory().openSession();
-
+            
             Criteria criteria1 = session.createCriteria(User.class);
             criteria1.add(Restrictions.eq("email", user_DTO.getEmail()));
             criteria1.add(Restrictions.eq("password", user_DTO.getPassword()));
-
+            
             if (!criteria1.list().isEmpty()) {
                 User user = (User) criteria1.list().get(0);
-
+                
                 if (!user.getVerification().equals("verified")) {
-                    
-                    //not verified
 
+                    //not verified
                     request.getSession().setAttribute("email", user_DTO.getEmail());
                     response_DTO.setContent("Not Verified");
-
-                } else {
                     
-                    //verified
+                } else {
 
+                    //verified
                     user_DTO.setFirst_name(user.getFirst_name());
                     user_DTO.setLast_name(user.getLast_name());
                     user_DTO.setPassword(null);
                     request.getSession().setAttribute("user", user_DTO);
 
+                    //Transfer Session cart to DB cart
+                    if (request.getSession().getAttribute("sessionCart") != null) {
+                        //Session cart Found
+
+                        ArrayList<Cart_DTO> sessionCart = (ArrayList<Cart_DTO>) request.getSession().getAttribute("sessionCart");
+                        
+                        Criteria criteria2 = session.createCriteria(Cart.class);
+                        criteria2.add(Restrictions.eq("user", user));
+                        List<Cart> dbCart = criteria2.list();
+                        
+                        if (dbCart.isEmpty()) {
+                            // DB cart is empty
+
+                            // Add all session cart items into DB cart
+                            for (Cart_DTO cart_DTO : sessionCart) {
+                                Cart cart = new Cart();
+                                cart.setProduct(cart_DTO.getProduct()); //****
+                                cart.setQty(cart_DTO.getQty());
+                                cart.setUser(user);
+                                session.save(cart);
+                            }
+                        } else {
+                            // Found items in DB cart
+
+                            for (Cart_DTO cart_DTO : sessionCart) {
+                                boolean isFoundInDBCart = false;
+                                
+                                for (Cart cart : dbCart) {
+                                    if (cart_DTO.getProduct().getId() == cart.getProduct().getId()) {
+                                        // Same item found in Session Cart & DB Cart
+                                        isFoundInDBCart = true;
+                                        
+                                        if ((cart_DTO.getQty() + cart.getQty()) <= cart.getProduct().getQty()) {
+                                            // Quantity available
+                                            cart.setQty(cart_DTO.getQty() + cart.getQty());
+                                            session.update(cart);
+                                        } else {
+                                            // Quantity not available
+                                            // Set max available qty
+                                            cart.setQty(cart.getProduct().getQty());
+                                            session.update(cart);
+                                        }
+                                        
+                                    }
+                                }
+                                
+                                if (!isFoundInDBCart) {
+                                    // Not found in DB Cart
+                                    Cart cart = new Cart();
+                                    cart.setProduct(cart_DTO.getProduct()); //***
+                                    cart.setQty(cart_DTO.getQty());
+                                    cart.setUser(user);
+                                    session.save(cart);
+                                    
+                                }
+                            }
+                            
+                        }
+                        
+                        request.getSession().removeAttribute("sessionCart");
+                        session.beginTransaction().commit();
+                        
+                    }
+                    
                     response_DTO.setSuccess(true);
                     response_DTO.setContent("Sign In Success");
                 }
-
+                
             } else {
                 response_DTO.setContent("invaild details!");
             }
         }
-
+        
         response.setContentType("application/json");
         response.getWriter().write(gson.toJson(response_DTO));
         System.out.println(gson.toJson(response_DTO));
-
+        
     }
     
 }
