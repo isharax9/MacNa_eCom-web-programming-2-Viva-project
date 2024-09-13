be0d0ad39f69388fb52a333223fdc57418c0e8ac
mac_knight141 <123813453+isharax9@users.noreply.github.com>
2024-09-13 13:06:19 +0530

    Search page stage 1 ✅
    📌checked all the required data can be fetch and display client side
    📌done catogory and condition selection part
    📌changes to seach.html page like onload function calling to body

--- src/java/controller/LoadData.java
+++ src/java/controller/LoadData.java
@@ -1,11 +1,23 @@
 package controller;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import entity.Category;
+import entity.Color;
+import entity.Product;
+import entity.Product_Condition;
+import entity.Storage;
 import java.io.IOException;
+import java.util.List;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import model.HibernateUtil;
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.criterion.Order;
 
 @WebServlet(name = "LoadData", urlPatterns = {"/LoadData"})
 public class LoadData extends HttpServlet {
@@ -13,10 +25,61 @@ public class LoadData extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-        System.out.println("Success");
-        
-        
-    }
+        //System.out.println("Success");
+        JsonObject jsonObject = new JsonObject();
+        jsonObject.addProperty("success", false);
 
+        Gson gson = new Gson();
+
+        Session session = HibernateUtil.getSessionFactory().openSession();
+
+        // main code
+        // Get Category list from DB
+        Criteria criteria = session.createCriteria(Category.class);
+        List<Category> categoryList = criteria.list();
+        jsonObject.add("categoryList", gson.toJsonTree(categoryList));
+
+        // Get Condition list from DB
+        Criteria criteria2 = session.createCriteria(Product_Condition.class);
+        List<Product_Condition> conditionList = criteria2.list();
+        jsonObject.add("conditionList", gson.toJsonTree(conditionList));
+
+        // Get Color list from DB
+        Criteria criteria3 = session.createCriteria(Color.class);
+        List<Color> colorList = criteria3.list();
+        jsonObject.add("colorList", gson.toJsonTree(colorList));
+
+        // Get Storage list from DB
+        Criteria criteria4 = session.createCriteria(Storage.class);
+        List<Storage> storageList = criteria4.list();
+        jsonObject.add("storageList", gson.toJsonTree(storageList));
+
+        // Get Product list from DB
+        Criteria criteria5 = session.createCriteria(Product.class);
+
+        // Get latest Products
+        criteria5.addOrder(Order.desc("id"));
+        jsonObject.addProperty("allproductCount", criteria5.list().size());
+
+        // Set Product Range
+        criteria5.setFirstResult(1);
+        criteria5.setMaxResults(6);
+
+        List<Product> productList = criteria5.list();
+
+        //Removing user's from product json object
+        for (Product product : productList) {
+            product.setUser(null);
+        }
+
+        jsonObject.add("productList", gson.toJsonTree(productList));
+        jsonObject.addProperty("success", true);
+        
+        
+        // main code
+        response.setContentType("application/json");
+        response.getWriter().write(gson.toJson(jsonObject));
+
+    }
 
 }
--- web/js/search.js
+++ web/js/search.js
@@ -6,6 +6,75 @@ async function loadData() {
     if (response.ok) {
         const json = await response.json();
         console.log(json);
+
+
+        //Start: load Category List
+        let category_options = document.getElementById("category-options");
+
+        let category_li = document.getElementById("category-li");
+        category_options.innerHTML = "";
+
+        let categoryList = json.categoryList;
+        categoryList.forEach(category => {
+            let category_li_clone = category_li.cloneNode(true);
+            category_li_clone.querySelector("#category-a").innerHTML = category.name;
+            category_options.appendChild(category_li_clone);
+        });
+
+        //start template js
+        const categoryOptions = document.querySelectorAll('#category-options li');
+        categoryOptions.forEach(option => {
+            option.addEventListener('click', function () {
+                categoryOptions.forEach(opt => opt.classList.remove('current-cat'));
+                this.classList.add('current-cat');
+            });
+        });
+        //end template js
+
+        //End: load Category List
+
+
+
+        //Start: load Condition List
+        let condition_options = document.getElementById("condition-options");
+
+        let condition_li = document.getElementById("condition-li");
+        condition_options.innerHTML = "";
+
+        let conditionList = json.conditionList;
+        conditionList.forEach(condition => {
+            let condition_li_clone = condition_li.cloneNode(true);
+            condition_li_clone.querySelector("#condition-a").innerHTML = condition.name;
+            condition_options.appendChild(condition_li_clone);
+        });
+
+        //start template js 
+
+        const conditionOptions = document.querySelectorAll('#condition-options li');
+        conditionOptions.forEach(option => {
+            option.addEventListener('click', function () {
+                conditionOptions.forEach(opt => opt.classList.remove('chosen'));
+                this.classList.add('chosen');
+            });
+        });
+
+        //end template js 
+
+
+//End: load Condition List
+
+
+
+
+
+
+
+
+
+
+
+
+
     } else {
         popup.error({
             message: "Try again later!"
